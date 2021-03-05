1491dcfa43639cd67f19458b6c6cb8c79b695346
chanjm7 <dlcksals77@gmail.com>
2021-03-05 21:52:42 +0900

    feat: add typing game round 타이핑 게임에 라운드수를 정하고 5라운드가 되면 종료되게 만들었다

--- typing.py
+++ typing.py
@@ -1,14 +1,19 @@
 import string
 import random
 
-length = random.randint(3,7)
-answer = ""
-for i in range(length):
-    answer += random.choice(string.ascii_lowercase)
 
-print(answer)
-user_input = input('입력: ')
-if user_input == answer:
-    print('굿굿굿')
-else:
-    print('오타ㅠㅠ')
+round = 1
+while round != 5+1:
+    length = random.randint(3,7)
+    answer = ""
+    for i in range(length):
+        answer += random.choice(string.ascii_lowercase)
+    print('------{}라운드------'.format(round))
+    print(answer)
+    user_input = input('입력: ')
+    if user_input == answer:
+        print('굿굿굿')
+    else:
+        print('오타ㅠㅠ')
+    
+    round += 1
