d5c4c4d84e9b364db6dfe618b41a3eb358ac97fd
chanjm7 <dlcksals77@gmail.com>
2021-03-06 16:20:26 +0900

    feat: add correct count and wrong count 맞은 개수와 틀린개수를 저장하고 게임 끝나면 출력한다

--- typing.py
+++ typing.py
@@ -3,6 +3,8 @@ import random
 
 
 round = 1
+correct_count = 0
+wrong_count = 0
 while round != 5+1:
     length = random.randint(3,7)
     answer = ""
@@ -13,8 +15,11 @@ while round != 5+1:
     user_input = input('입력: ')
     if user_input == answer:
         print('굿굿굿')
+        correct_count += 1
     else:
         print('오타ㅠㅠ')
-    
+        wrong_count += 1
     round += 1
-
+print('------게임끝------')
+print('맞은개수: {}'.format(correct_count))
+print('틀린개수: {}'.format(wrong_count))
