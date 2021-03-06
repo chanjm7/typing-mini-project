77dbd60ac325a72107f167dba1e756fc36630010
chanjm7 <dlcksals77@gmail.com>
2021-03-06 18:25:53 +0900

    feat: add insert data fix: unused id, so change name in the table 데이터 삽입 기능을 만들고 테이블에 id보단 user의 name을 받는게 나을거 같아서 바꿨다

--- typing.py
+++ typing.py
@@ -10,11 +10,12 @@ conn = sqlite3.connect('/home/chanjmw35/Documents/dev/typing-mini-project/typing
 #데이터베이스 커서생성
 c = conn.cursor()
 #테이블 생성 
-c.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, correct_conut text, wrong_count text, time_taken text, regdate text)")
-#게임 정보
+c.execute("CREATE TABLE IF NOT EXISTS users(name text, correct_count text, wrong_count text, time_taken text, regdate text)")
+#게임 및 유저정보
 game_round = 1
 correct_count = 0
 wrong_count = 0
+user_name = input('저장할 이름을 정해주세요: ')
 
 #현재시간
 now = datetime.datetime.now()
@@ -35,7 +36,14 @@ while game_round != 5+1:
         print('오타ㅠㅠ')
         wrong_count += 1
     game_round += 1
+time_taken = round(time.time() - start_time, 2)
 print('------게임끝------')
-print('걸린시간: {}초'.format(round(time.time() - start_time, 2)))
+print('걸린시간: {}초'.format(time_taken))
 print('맞은개수: {}'.format(correct_count))
 print('틀린개수: {}'.format(wrong_count))
+
+#user데이터 삽입
+c.execute("INSERT INTO users(name, correct_count, wrong_count, time_taken, regdate) VALUES (?, ?, ?, ?, ?)", (user_name, correct_count, wrong_count, time_taken, nowDatetime))
+
+
+
