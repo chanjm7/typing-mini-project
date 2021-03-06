dd9011cc4b248ec307f71694c2f2ed0667295dd9
chanjm7 <dlcksals77@gmail.com>
2021-03-06 17:08:52 +0900

    feat: add total time taken fix: duplicate function and variable names round() 함수와 변수 round의 이름이 겹쳐서 생긴 문제 해결 round 변수를 game_round로 바꿨다

--- typing.py
+++ typing.py
@@ -2,20 +2,24 @@ import string
 import random
 import sqlite3
 import datetime
+import time
 
 
 now = datetime.datetime.now()
 nowDatetime = now.strftime('%y-%m-%d %H:%M:%S')
 print(nowDatetime)
-round = 1
+game_round = 1
 correct_count = 0
 wrong_count = 0
-while round != 5+1:
+start_time = time.time()
+print(type(start_time))
+
+while game_round != 5+1:
     length = random.randint(3,7)
     answer = ""
     for i in range(length):
         answer += random.choice(string.ascii_lowercase)
-    print('------{}라운드------'.format(round))
+    print('------{}라운드------'.format(game_round))
     print(answer)
     user_input = input('입력: ')
     if user_input == answer:
@@ -24,7 +28,8 @@ while round != 5+1:
     else:
         print('오타ㅠㅠ')
         wrong_count += 1
-    round += 1
+    game_round += 1
 print('------게임끝------')
+print('걸린시간: {}'.format(round(time.time() - start_time, 2)))
 print('맞은개수: {}'.format(correct_count))
 print('틀린개수: {}'.format(wrong_count))
