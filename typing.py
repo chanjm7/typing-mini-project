3e03cb7d1edb529e7fb2fc5dc9946b0abb577710
chanjm7 <dlcksals77@gmail.com>
2021-03-06 17:39:10 +0900

    feat: edit typing_game_data.db and database table 같은 경로에 데이터파일을 만들고 테이블도 지정했다

--- typing.py
+++ typing.py
@@ -5,15 +5,21 @@ import datetime
 import time
 
 
-now = datetime.datetime.now()
-nowDatetime = now.strftime('%y-%m-%d %H:%M:%S')
-print(nowDatetime)
+#데이터 베이스생성
+conn = sqlite3.connect('/home/chanjmw35/Documents/dev/typing-mini-project/typing_game_data.db', isolation_level=None)
+#데이터베이스 커서생성
+c = conn.cursor()
+#테이블 생성 
+c.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, correct_conut text, wrong_count text, time_taken text, regdate text)")
+#게임 정보
 game_round = 1
 correct_count = 0
 wrong_count = 0
-start_time = time.time()
-print(type(start_time))
 
+#현재시간
+now = datetime.datetime.now()
+nowDatetime = now.strftime('%y-%m-%d %H:%M:%S')
+start_time = time.time()
 while game_round != 5+1:
     length = random.randint(3,7)
     answer = ""
@@ -30,6 +36,6 @@ while game_round != 5+1:
         wrong_count += 1
     game_round += 1
 print('------게임끝------')
-print('걸린시간: {}'.format(round(time.time() - start_time, 2)))
+print('걸린시간: {}초'.format(round(time.time() - start_time, 2)))
 print('맞은개수: {}'.format(correct_count))
 print('틀린개수: {}'.format(wrong_count))
